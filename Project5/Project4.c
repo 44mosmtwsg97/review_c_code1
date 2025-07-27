6d3799934480dab4d41a4b1745b674597c3b9c23
44mosmtwsg97 <2926972979@qq.com>
2025-07-27 13:07:05 +0800

    递归
    不是特别明白，还得多练习

--- Project5/Project4.c
+++ Project5/Project4.c
@@ -3,47 +3,18 @@
 
 //有一头母牛，它每年年初生一头小母牛。每头小母牛从第四个年头开始，每年年初也生一头小母牛。
 //请编程实现在第n年的时候，共有多少头母牛？
-//int cattle(int year)
-//{
-//	if ( 0<year < 4)
-//		return year;
-//	else
-//		return cattle(year - 1) + cattle(year - 3);
-//}
-//int main()
-//{
-//	int year;
-//
-//	return 0;
-//}
-
-int Fuc(int K)
-
+int cattle(int year)
 {
-
-if (K == 1) return 1;
-
-if (K == 2) return 2;
-
-if (K == 3) return 3;
-
-
-
-return Fuc(K - 3) + Fuc(K - 1);
-
-
-
+	if (year == 1)return 1;
+	if (year == 2)return 2;
+	if (year == 3)return 3;
+	else
+		return cattle(year - 3) + cattle(year - 1);
 }
-
-
-
 int main()
-
 {
-
-int n,Num;
-
-Num = scanf("%d", &n);
-printf("%d", Num);
+	int year;
+	while (scanf("%d", &year))
+		printf("%d\n", cattle(year));
+	return 0;
 }
-
